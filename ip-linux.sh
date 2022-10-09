d5b11dd8b76f045b8d416ddb28c0b1a03e9911c5
aug9co <32621767+aug9co@users.noreply.github.com>
2022-10-09 11:31:31 +0200

    Update ip-linux.sh

--- ip-linux.sh
+++ ip-linux.sh
@@ -3,10 +3,10 @@
 #This script takes a list of IP addresses in a text file called "ip.txt", does a whois search, and greps for org-name, description, and country 
 #then outputs this to a file in csv format where pipe (|) is the delimiter
 
-IP="IP-Adresse"													#Define headlines variables
-OrgName="Organisasjonsnavn"											#Define headlines variables
-Descr="Beskrivelse"												#Define headlines variables
-Country="Land"													#Define headlines variables
+IP="IP address"													#Define headlines variables
+OrgName="Organisation name"
+Descr="Description"
+Country="Country"
 
 echo $IP\|$OrgName\|$Descr\|$Country > whois.csv								#Write headlines variables in csv-format
 
@@ -17,8 +17,8 @@ Lines=$(cat $File)												#Open file and read lines
 
 for ip in $Lines												#For each line in input file
 	do													#Do the following (Start of loop)
-		org=$(whois $ip | grep -i 'org-name\|orgname' | awk -F ':' '{print $2}' | xargs)		#Do a whois lookup, grep for org-name, and select the second value in the line, remove newlines. Define value as variable "org"
-		des=$(whois $ip | grep -i 'descr' | awk -F ':' '{print $2}' | xargs)				#Do a whois lookup, grep for descr, and select the second value in the line, remove newlines. Define value as variable "des"
-		country=$(whois $ip | grep -i 'country' | awk -F ':' '{print $2}' | xargs)			#Do a whois lookup, grep for country, and select the second value in the line, remove newlines. Define value as variable "country"
+		org=$(whois $ip | grep -i 'org-name\|orgname' | awk -F ':' '{print $2}' | xargs)		#Do a whois lookup, grep for org-name, and select the second value in the line, remove newlines. Define output as variable "org"
+		des=$(whois $ip | grep -i 'descr' | awk -F ':' '{print $2}' | xargs)				#Do a whois lookup, grep for descr, and select the second value in the line, remove newlines. Define output as variable "des"
+		country=$(whois $ip | grep -i 'country' | awk -F ':' '{print $2}' | xargs)			#Do a whois lookup, grep for country, and select the second value in the line, remove newlines. Define output as variable "country"
 		echo $ip\|$org\|$des\|$country >> whois.csv							#Write the variables ip, org, des, and country to a file in csv-format
 done														#End of loop
